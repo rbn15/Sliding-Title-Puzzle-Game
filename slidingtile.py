8ae21b4e47d6d3f267e3e517b09acb24552d3a00
rbn15 <prasoon.nath15@gmail.com>
2021-10-21 13:54:25 -0400

    Adds the condition to check input sanity

--- slidingtile.py
+++ slidingtile.py
@@ -1,7 +1,18 @@
+import sys
 #defining cmp
 def cmp(a,b):
-	return (a>b)-(a<b)
+    return (a>b)-(a<b)
 
+def sanity_check(a):
+   # if isinstance(a, int):
+    if a>'0' and a<'9':
+        return int(a)
+    elif a==' ':
+        return a
+    else:
+        return -1
+
+#print(sanity_check(-1))
 
 print("*****************************")
 print("*3x3 sliding puzzle game")
@@ -19,28 +30,38 @@ s=[]
 #input
 # need to add a condition to identify numbers and a check if numbers are <9 
 
-for i in range(3):
-	e=input()
-	s=[]
-	for j in e:
-		s.append(j)
-	a.append(s)	
+def input1():
+    global a
+    s=[]
+    sane=0
+    for i in range(3):
+        e=input()
+        s=[]
+        for j in e:
+            sane=sanity_check(j)
+    #        print("sanity",j,sane)
+            if sane==-1:
+                sys.exit("input not valid")
+            s.append(j)
+       # if sane==-1:
+        #    sys.exit("Input not valid")
+        a.append(s) 
 
 ci=4
 cj=4
 def prnt():
-	global cj,ci
-	for i in range(3):
-	     for j in range(3):
-		     print("|",a[i][j], end='|')
-		     if a[i][j]==' ':
-	     		ci=i
-	     		cj=j
-	     print()
-	print()     
-	
-	     
-
+    global cj,ci
+    for i in range(3):
+         for j in range(3):
+             print("|",a[i][j], end='|')
+             if a[i][j]==' ':
+                ci=i
+                cj=j
+         print()
+    print()     
+    
+         
+input1()
 prnt()
 #print("ci=",ci,"cj=",cj)
 
@@ -48,60 +69,60 @@ prfta=[['1','2','3'],['4','5','6'],['7','8',' ']]
 #condition check
 
 def check():
-	global ci , cj
-	if cmp(a,prfta)==0 :
-	     print("********Perfect!!!!!******")
-	     return False
-	if  ci==4 :
-		print("Sorry but you cant solve this puzzle")
-		return False
+    global ci , cj
+    if cmp(a,prfta)==0 :
+         print("********Perfect!!!!!******")
+         return False
+    if  ci==4 :
+        print("Sorry but you cant solve this puzzle")
+        return False
 
-	else :
-		return True
-	     
+    else :
+        return True
+         
 def right(a):
-	global cj
-	if cj!=0:
-		t=a[cj-1]
-		a[cj-1]=' '
-		a[cj]=t
+    global cj
+    if cj!=0:
+        t=a[cj-1]
+        a[cj-1]=' '
+        a[cj]=t
 
 def left(a):
-	global cj
-	if cj!=2:
-		t=a[cj+1]
-		a[cj+1]=' '
-		a[cj]=t
+    global cj
+    if cj!=2:
+        t=a[cj+1]
+        a[cj+1]=' '
+        a[cj]=t
 
 
 
 while check():
-	prnt()
-	#print("ci=",ci,"cj=",cj)
-	cm=input()
-	if cm=='d':
-		right(a[ci])
+    prnt()
+    #print("ci=",ci,"cj=",cj)
+    cm=input()
+    if cm=='d':
+        right(a[ci])
 
-	if cm=='a' :
-		left(a[ci])
+    if cm=='a' :
+        left(a[ci])
 
-	if cm=='w':
-		#global ci , cj
-		if ci!=2:
-			t=a[ci+1][cj]
-			a[ci+1][cj]=' '
-			a[ci][cj]=t
+    if cm=='w':
+        #global ci , cj
+        if ci!=2:
+            t=a[ci+1][cj]
+            a[ci+1][cj]=' '
+            a[ci][cj]=t
 
-	if cm=='s' :
-		#global ci, cj
-		if ci!=0:
-			t=a[ci-1][cj]
-			a[ci-1][cj]=' '
-			a[ci][cj]=t
-	if cm=='giveup':
-		print("Its not a good habit!!!")
-		break
-			
+    if cm=='s' :
+        #global ci, cj
+        if ci!=0:
+            t=a[ci-1][cj]
+            a[ci-1][cj]=' '
+            a[ci][cj]=t
+    if cm=='giveup':
+        print("Its not a good habit!!!")
+        break
+            
 
 
 
